7f2a765973a32bf467cbb310945199f2915384f7
ju5tu5 <justus@ju5tu5.nl>
2021-04-12 22:56:47 +0200

    created the example

---
+++ script.js
@@ -0,0 +1,35 @@
+'use strict'
+
+function publishSubscribePattern () {
+  const subscriptions = {}
+
+  function publish (eventName, data) {
+    if (!Array.isArray(subscriptions[eventName])) {
+      return
+    }
+    subscriptions[eventName].forEach((callback) => { callback(data) })
+  }
+
+  function subscribe (eventName, callback) {
+    if (!Array.isArray(subscriptions[eventName])) {
+      subscriptions[eventName] = []
+    }
+    subscriptions[eventName].push(callback)
+
+    return function unsubscribe () {
+      subscriptions[eventName].splice(subscriptions[eventName].length - 1, 1)
+    }
+  }
+
+  return {
+    publish,
+    subscribe
+  }
+}
+
+function messageHandler (message) {
+  console.log('> ', message)
+}
+
+const pubsub = publishSubscribePattern()
+const unsubscribe = pubsub.subscribe('message', messageHandler)
